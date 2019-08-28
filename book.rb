dc8829a8a16594ec48bc9e8a8edfa3907c130be5
David Strauch <dmaster18@gmail.com>
2019-08-28 19:45:50 +0000

    Automatically backed up by Learn

--- book.rb
+++ book.rb
@@ -30,7 +30,7 @@ angels = Book.new("Angels & Demons", "Action")
 pride = Book.new("Pride & Prejudice", "Drama")
 macbeth = Book.new("Macbeth", "Shakespearean Drama")
 
-new_hash = {:name => "address, 
+new_hash = {name: => "address, 
 
 puts macbeth.genre
 puts pride.genre_count
