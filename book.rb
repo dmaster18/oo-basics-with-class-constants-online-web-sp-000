96c66d7363889fa62855a045ddc511f4a7aa38be
David Strauch <dmaster18@gmail.com>
2019-08-28 19:46:45 +0000

    Automatically backed up by Learn

--- book.rb
+++ book.rb
@@ -32,7 +32,7 @@ macbeth = Book.new("Macbeth", "Shakespearean Drama")
 
 new_hash = {first_name: "Bob", last_name: "Dole"}
 
-puts new_hash
+puts new_hash[:first_name]
 
 puts macbeth.genre
 puts pride.genre_count
